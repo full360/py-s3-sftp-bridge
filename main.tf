1019dfcd8dbb8550f36c13cc13961c4c4d6de29c
Rohit Amarnath <rohit.amarnath@full360.com>
2018-01-23 13:10:53 -0500

    fix how make is called

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 data "external" "get_filename" {
-  program = ["bash", "-c", "${path.module}/make", "terraform_zip" ]
+  program = ["bash", "-c", "cd ${path.module} && make docker > ./build.log 2>&1 && jq -n --arg zip_file s3-sftp-bridge.zip '{\"zip_file\":$zip_file}'" ]
 }
